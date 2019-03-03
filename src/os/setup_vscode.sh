24f15f93e67bcf0dfccdca93c515fedebd2196d5
Simon Siefke <simon.siefke@gmail.com>
2019-03-03 23:48:26 +0100

    setup vscode

--- src/os/setup_vscode.sh
+++ src/os/setup_vscode.sh
@@ -8,7 +8,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 setup_vscode() {
     if code --list-extensions &> /dev/null; then
       code --install-extension Shan.code-settings-sync 
-      echo "{ \"sync.gist\": \"8326ca592e1060d63a2b6d6d721fc3e7\",\"sync.autoDownload\": true,}\"" > /home/simon/.config/Code/User/settings.json
+      echo "{ \"sync.gist\": \"8326ca592e1060d63a2b6d6d721fc3e7\",\"sync.autoDownload\": true}" > /home/simon/.config/Code/User/settings.json
     fi
 }
 
@@ -17,7 +17,7 @@ setup_vscode() {
 setup_vscode_insiders() {
     if code-insiders --list-extensions &> /dev/null; then
       code-insiders --install-extension Shan.code-settings-sync 
-      echo "{ \"sync.gist\": \"8326ca592e1060d63a2b6d6d721fc3e7\",\"sync.autoDownload\": true,}\"" > /home/simon/.config/Code-Insiders/User/settings.json
+      echo "{ \"sync.gist\": \"8326ca592e1060d63a2b6d6d721fc3e7\",\"sync.autoDownload\": true}" > /home/simon/.config/Code-Insiders/User/settings.json
 
     fi
 }
