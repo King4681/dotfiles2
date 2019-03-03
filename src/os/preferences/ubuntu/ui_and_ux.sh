57af8e9691d46d6bdab17c872a1266ef4dd0ccd7
Simon Siefke <simon.siefke@gmail.com>
2019-03-03 21:07:45 +0100

    no screensaver

--- src/os/preferences/ubuntu/ui_and_ux.sh
+++ src/os/preferences/ubuntu/ui_and_ux.sh
@@ -11,6 +11,8 @@ execute "gsettings set org.gnome.libgnomekbd.keyboard layouts \"[ 'us', 'ro' ]\"
     "Set keyboard languages"
 execute "gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'" \
     "Enable minimize window on click"
+execute "gsettings set org.gnome.desktop.screensaver ubuntu-lock-on-suspend 'false'" \
+    "Disable Lockscreen"
 # execute "gsettings set com.canonical.Unity.Launcher favorites \"[
 #             'ubiquity-gtkui.desktop',
 #             'nautilus-home.desktop'
