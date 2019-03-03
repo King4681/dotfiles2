3f73d3089e7641a4c4f39c6d4f7db3510ef3819e
Simon Siefke <simon.siefke@gmail.com>
2019-03-03 21:06:46 +0100

    ui

--- src/os/preferences/ubuntu/ui_and_ux.sh
+++ src/os/preferences/ubuntu/ui_and_ux.sh
@@ -9,7 +9,8 @@ print_in_purple "\n   UI & UX\n\n"
 
 execute "gsettings set org.gnome.libgnomekbd.keyboard layouts \"[ 'us', 'ro' ]\"" \
     "Set keyboard languages"
-
+execute "gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'" \
+    "Enable minimize window on click"
 # execute "gsettings set com.canonical.Unity.Launcher favorites \"[
 #             'ubiquity-gtkui.desktop',
 #             'nautilus-home.desktop'
