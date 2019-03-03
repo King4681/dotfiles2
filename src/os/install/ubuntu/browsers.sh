1c4cc4c5e2cc67305dcc8618cea5c9cbe0db5e8f
Simon Siefke <simon.siefke@gmail.com>
2019-03-03 21:23:30 +0100

    add chrome

--- src/os/install/ubuntu/browsers.sh
+++ src/os/install/ubuntu/browsers.sh
@@ -13,3 +13,4 @@ print_in_purple "\n   Browsers\n\n"
 printf "\n"
 
 install_package "Flash" "flashplugin-installer"
+install_command "Chrome" "wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo apt install ./google-chrome*.deb"
