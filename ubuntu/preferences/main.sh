7ed1ae37ee76531bb06a6dc25922c0a4e698d921
s.siefke.ext@bitworkers.eu <simon.siefke@gmail.com>
2019-03-04 17:47:46 +0100

    move

--- ubuntu/preferences/main.sh
+++ ubuntu/preferences/main.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-cd "$(dirname "${BASH_SOURCE[0]}")"
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../utils.sh"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+print_in_purple "\n • Preferences\n"
 
 ./privacy.sh
 ./terminal.sh
