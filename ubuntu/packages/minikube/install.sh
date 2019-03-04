e62bc298a1a7cdf6f8955139543925ad59a35e00
s.siefke.ext@bitworkers.eu <simon.siefke@gmail.com>
2019-03-04 18:50:46 +0100

    update minikube

--- ubuntu/packages/minikube/install.sh
+++ ubuntu/packages/minikube/install.sh
@@ -3,3 +3,4 @@
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 chmod +x minikube
 sudo cp minikube /usr/local/bin && rm minikube
+eval $(minikube docker-env)
