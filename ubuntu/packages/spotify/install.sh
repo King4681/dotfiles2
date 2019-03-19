f78a514dbc9979f9f77984a8e7eae177a31ddbad
Simon Siefke <simon.siefke@gmail.com>
2019-03-19 11:10:52 +0100

    Update install.sh

--- ubuntu/packages/spotify/install.sh
+++ ubuntu/packages/spotify/install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 sudo snap install -y spotify
+
+
+# Disable notifications
+# Exit Spotify, then edit ~/.config/spotify/Users/[Spotify user name]-user/prefs and set ui.track_notifications_enabled=false
